f505e2ec135606af61c433c0a5d33a6107dd996d
Rangga Priago <141876195+RPriago@users.noreply.github.com>
2025-12-12 15:35:47 +0700

    Mengganti Model

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 )
 
 # --- KONFIGURASI & LOAD MODEL ---
-MODEL_PATH = "models/modelCapstoneAsahLeadScoring.joblib"
+MODEL_PATH = "models/model_bank_lead_scoring.joblib"
 MODEL_THRESHOLD = 0.5 
 model = None
 
